d483dc02000f75a1cda6a93603db36b5d5b1a974
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-06-17 16:09:57 -0300

    Criação do endpoint de Veiculo

--- demo/src/main/java/br/com/locatech/locatech/controllers/VeiculoController.java
+++ demo/src/main/java/br/com/locatech/locatech/controllers/VeiculoController.java
@@ -1,4 +1,16 @@
 package br.com.locatech.locatech.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/veiculos")
 public class VeiculoController {
+
+    @GetMapping
+    public String getVeiculos(){
+        return "hello veiculo";
+    }
 }
