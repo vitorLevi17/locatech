102e3e0fb294023d1e939c01b09d3d5922613f58
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-06-18 11:03:19 -0300

    Service do veiculo

--- demo/src/main/java/br/com/locatech/locatech/repository/VeiculoRepositoryImp.java
+++ demo/src/main/java/br/com/locatech/locatech/repository/VeiculoRepositoryImp.java
@@ -4,7 +4,6 @@ import br.com.locatech.locatech.entitys.Veiculo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
---
+++ demo/src/main/java/br/com/locatech/locatech/services/VeiculoService.java
@@ -0,0 +1,46 @@
+package br.com.locatech.locatech.services;
+
+import br.com.locatech.locatech.entitys.Veiculo;
+import br.com.locatech.locatech.repository.VeiculoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VeiculoService {
+    private final VeiculoRepository veiculoRepository;
+
+    public VeiculoService(VeiculoRepository veiculoRepository) {
+        this.veiculoRepository = veiculoRepository;
+    }
+
+    public List<Veiculo> findAllVeiculos(int page, int size){
+        int offset = (page-1) * size;
+        return this.veiculoRepository.findyAll(size,offset);
+    }
+    public Optional<Veiculo> findVeiculoById(Long id){
+        return this.veiculoRepository.findById(id);
+    }
+    public void saveVeiculo(Veiculo veiculo){
+        var save = this.veiculoRepository.save(veiculo);
+        //Verificar se só 1 linha foi afetada e mandar msg de erro
+        Assert.state(save == 1,"Erro ao salvar veiculo" + veiculo.getModelo());
+    }
+
+    public void updateVeiculo(Veiculo veiculo,Long id){
+        var update = this.veiculoRepository.update(veiculo,id);
+        if (update == 0){
+            throw new RuntimeException("Veiculo não encontrado");
+        }
+    }
+    public void deleteVeiculo(Long id){
+        var delete = this.veiculoRepository.delete(id);
+        if (delete == 0){
+            throw new RuntimeException("Veiculo não encontrado");
+        }
+    }
+
+}
