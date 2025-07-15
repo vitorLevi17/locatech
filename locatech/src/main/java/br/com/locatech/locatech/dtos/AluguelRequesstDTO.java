dbeadb3576764bc51d9720dc77b120f4dafc0efe
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-07-15 15:53:27 -0300

    fix: validação da data do aluguel
    Validar a data do aluguel para ser não nula

--- locatech/src/main/java/br/com/locatech/locatech/dtos/AluguelRequesstDTO.java
+++ locatech/src/main/java/br/com/locatech/locatech/dtos/AluguelRequesstDTO.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 public record AluguelRequesstDTO(@NotNull(message = "O campo (pessoa_id) não pode ser nulo") Long pessoa_id,
                                  @NotNull(message = "O campo (veiculo_id) não pode ser nulo")Long veiculo_id,
-                                 @NotBlank(message = "O campo (data_inicio) não pode ser nulo") LocalDate data_inicio,
-                                 @NotBlank(message = "O campo (data_final) não pode ser nulo") LocalDate data_final
+                                 @NotNull(message = "O campo (data_inicio) não pode ser nulo") LocalDate data_inicio,
+                                 @NotNull(message = "O campo (data_final) não pode ser nulo") LocalDate data_final
                                  ) {
 }
